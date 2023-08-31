1f3f79a22384dab7d79279bd5aaba45270cf7f35
MarcosCunhaDev <marcoswrc2@gmail.com>
2023-08-31 18:14:42 -0300

    Teste de rodar o deploy sozinho

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Teste de atualização automática do deploy</h1>
+      <h1>Teste de deploy do marcos</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
