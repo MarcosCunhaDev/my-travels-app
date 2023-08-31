da99714c8237c05e53c0698bfa3adb036f7fce19
MarcosCunhaDev <marcoswrc2@gmail.com>
2023-08-31 18:08:45 -0300

    Apenas um teste do deploy

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Fiz uma pequena mudança aqui</h1>
+      <h1>Teste de atualização automática do deploy</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
