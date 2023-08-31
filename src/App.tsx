8840e136ee06d285caff18ba49123750b19b461e
MarcosCunhaDev <marcoswrc2@gmail.com>
2023-08-31 18:28:17 -0300

    Ajustes de layout e configs

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Teste de deploy do marcos</h1>
+      <h1>App com CI/CD</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
